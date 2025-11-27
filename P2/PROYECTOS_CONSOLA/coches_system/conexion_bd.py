b78ff109cf4d1fdfde65e2e5904f3da8a6025fb4
Bibiana Soria <bibiana_3141240181@utd.edu.mx>
2025-11-26 19:15:28 -0600

    coches_system

--- P2/PROYECTOS_CONSOLA/coches_system/conexion_bd.py
+++ P2/PROYECTOS_CONSOLA/coches_system/conexion_bd.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql.connector
 
 try:
@@ -13,18 +12,3 @@ try:
 except:
     print("Ocurrio un error con la base de datos... Verifique")
 
-=======
-import mysql.connector
-
-try:
-    conexion=mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="",
-    database="coches"
-    )
-    cursor=conexion.cursor(buffered=True)
-except:
-    print("Ocurrio un error con la base de datos... Verifique")
-
->>>>>>> b089ef6c5e532afcf47601aa1b79a78263122bd4
